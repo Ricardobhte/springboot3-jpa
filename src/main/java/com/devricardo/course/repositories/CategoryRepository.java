a3f497776fa69302eb66cad408682e287a28bd24
Ricardo Camillo <ricardobhte@hotmail.com>
2024-03-06 14:21:23 -0300

    Category

---
+++ src/main/java/com/devricardo/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.devricardo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devricardo.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
