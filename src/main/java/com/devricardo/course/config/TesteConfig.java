2329b8c2cbe61afb70324d4325590a7704ddb00c
Ricardo Camillo <ricardobhte@hotmail.com>
2024-03-11 13:50:12 -0300

    Payment, one-to-one association

--- src/main/java/com/devricardo/course/config/TesteConfig.java
+++ src/main/java/com/devricardo/course/config/TesteConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.devricardo.course.entities.Category;
 import com.devricardo.course.entities.Order;
 import com.devricardo.course.entities.OrderItem;
+import com.devricardo.course.entities.Payment;
 import com.devricardo.course.entities.Product;
 import com.devricardo.course.entities.User;
 import com.devricardo.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
-		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
